15a34537ac3c649465ac84e26c8cad19055573f5
Malaury Keslick <malaury.keslick@soprasteria.com>
2025-07-21 21:50:06 +0200

    Revert all changes made in sendMail logic

--- frontend/functions/sendMail.js
+++ frontend/functions/sendMail.js
@@ -1,8 +1,7 @@
 const nodemailer = require('nodemailer');
 
-exports.handler = async function(event, context) {
+exports.handler = async function(event) {
   const { name, email, subject, message } = JSON.parse(event.body || '{}');
-  console.log("Received a request from " + name + " to send an email.");
 
   if (!name || !email || !message) {
     return {
@@ -24,16 +23,14 @@ exports.handler = async function(event, context) {
       from: email,
       to: process.env.EMAIL_USER,
       subject: `[PORTFOLIO] ${subject}`,
-      text: `[Sent by ${name}/${email}]\n${message}`
+      text: `[Sent by ${name}@${email}]\n${message}`
     });
 
-    console.log("Message sent!");
     return {
       statusCode: 200,
       body: JSON.stringify({ success: true })
     };
   } catch (error) {
-    console.log("[ERROR] An error occurred : " + error.message);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message })
