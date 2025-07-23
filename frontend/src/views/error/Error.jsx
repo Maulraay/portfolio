a7489eb3a999024d6ee8f1b649745f2b11aa2d4b
Malaury Keslick <malaury.keslick@soprasteria.com>
2025-07-23 13:26:28 +0200

    Error page update + responsive

--- frontend/src/views/error/Error.jsx
+++ frontend/src/views/error/Error.jsx
@@ -1,37 +1,41 @@
 import React from "react";
-import {Layout} from "../layout/Layout";
+import { Layout } from "../layout/Layout";
 import { Typography } from "@mui/material";
-import { WarningTwoTone, TrendingFlatTwoTone } from "@mui/icons-material";
+import { FormattedMessage, useIntl } from "react-intl";
 
 const Error = () => {
+  const intl = useIntl();
+
   return (
     <Layout>
-      <div className={'body'}>
-        <Typography fontSize={50} className={'errorTypography'}>
-          <WarningTwoTone className={'icon'} fontSize={"inherit"}/>
-          Oops! Something went wrong.
-        </Typography>
-        <Typography className={'errorTypography'}>
-          It looks like our server isn’t responding at the moment. This might be due to high traffic or a temporary issue on our end.
-        </Typography>
-        <Typography className={'errorTypography'}>
-          Here are a few things you can try:
-        </Typography>
-        <Typography className={'errorTypography'}>
-          <TrendingFlatTwoTone className={'icon'}/>
-          Refresh the page after a few moments.
-        </Typography>
-        <Typography className={'errorTypography'}>
-          <TrendingFlatTwoTone className={'icon'}/>
-          Check your internet connection.
-        </Typography>
-        <Typography className={'errorTypography'}>
-          <TrendingFlatTwoTone className={'icon'}/>
-          If the issue persists, please contact our support team.
-        </Typography>
-        <Typography className={'errorTypography'}>
-          We apologize for the inconvenience and appreciate your patience!
-        </Typography>
+      <div className={'error'}>
+        <img src={"./assets/error.webp"} className={""} alt={intl.formatMessage({id: "error.img_alt", defaultMessage: "'OOPS' illustration"})}/>
+        <div className={'errorText'}>
+          <Typography variant={"projects"} className={'errorTypography'}>
+            <FormattedMessage id={"error.title"} defaultMessage={"Something went wrong..."}/>
+          </Typography>
+          <Typography variant={"body1"} className={'errorTypography'}>
+            <FormattedMessage id={"error.content_1"} defaultMessage={"It looks like this page isn’t responding at the moment. This might be due to high traffic or a temporary issue on my end."}/>
+          </Typography>
+          <Typography variant={"body1"} className={'errorTypography'}>
+            <FormattedMessage id={"error.content_2"} defaultMessage={"Here are a few things you can try :"}/>
+          </Typography>
+          <Typography variant={"body1"} className={'errorTypography'}>
+            <FormattedMessage id={"error.content_3"} defaultMessage={"• Refresh the page after a few moments."}/>
+          </Typography>
+          <Typography variant={"body1"} className={'errorTypography'}>
+            <FormattedMessage id={"error.content_4"} defaultMessage={"• Check your internet connection."}/>
+          </Typography>
+          <Typography variant={"body1"} className={'errorTypography'}>
+            <FormattedMessage id={"error.content_5_1"} defaultMessage={"• If the issue persists, please "}/>
+            <a href={"https://github.com/Maulraay/portfolio/issues/new"} target={"_blank"} rel={"noopener noreferrer"} className={"link"}><FormattedMessage id={"error.content_5_2"} defaultMessage={"fill an issue"}/></a>
+            <FormattedMessage id={"error.content_5_3"} defaultMessage={" or contact me at "}/>
+            <a href={"mailto:mkeslick.pro@gmail.com"} target={"_blank"} rel={"noopener noreferrer"} className={"link"}>mkeslick.pro@gmail.com</a>.
+          </Typography>
+          <Typography variant={"body1"} className={'errorTypography'}>
+            <FormattedMessage id={"error.content_6"} defaultMessage={"I apologize for the inconvenience and appreciate your patience!"}/>
+          </Typography>
+        </div>
       </div>
     </Layout>
   )
